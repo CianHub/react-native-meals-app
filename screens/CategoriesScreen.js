e240963c65e398ef34b59f63295bc1a7f4449329
Cian <ihatecian@gmail.com>
2022-08-28 13:28:30 +0100

    add custom category tile

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
+import { CategoryGridTile } from '../components/CategoryGridTile';
 import { CATEGORIES } from '../data/dummy-data';
 
+const renderCategoryItem = (itemData) => {
+  return <CategoryGridTile category={itemData} />;
+};
+
 export const CategoriesScreen = () => {
-  return <View>CategoriesScreen</View>;
+  return (
+    <FlatList data={CATEGORIES} keyExtractor={(item) => item.id}>
+      {renderCategoryItem}
+    </FlatList>
+  );
 };
