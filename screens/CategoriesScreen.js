740ddba4fbf6382fdccbada39b645ae3cd0d6064
Cian <ihatecian@gmail.com>
2022-08-28 13:23:00 +0100

    add categories creen file

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+
+export const CategoriesScreen = () => {
+  return <View>CategoriesScreen</View>;
+};
