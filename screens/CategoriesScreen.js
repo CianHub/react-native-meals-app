63f0858b2046097ec9354c73eff0195e0d748e93
Cian <ihatecian@gmail.com>
2022-08-28 13:31:49 +0100

    cleanup code

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import { CategoryGridTile } from '../components/CategoryGridTile';
 import { CATEGORIES } from '../data/dummy-data';
 
 const renderCategoryItem = (itemData) => {
-  return <CategoryGridTile category={itemData} />;
+  return <CategoryGridTile title={itemData.title} color={itemData.color} />;
 };
 
 export const CategoriesScreen = () => {
   return (
-    <FlatList data={CATEGORIES} keyExtractor={(item) => item.id}>
-      {renderCategoryItem}
-    </FlatList>
+    <FlatList
+      renderItem={renderCategoryItem}
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+    />
   );
 };
